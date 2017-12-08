92a08b63e07cdb7cc1513dfec446bc335b57e3e1
Michael Fogleman <michaelwfogleman@gmail.com>
2017-12-08 15:51:03 -0500

    Initial basic literate configuration.

---
+++ init.el
@@ -0,0 +1,15 @@
+;; This sets up the load path so that we can override it
+(package-initialize nil)
+
+;; Load the rest of the packages
+(package-initialize t)
+(setq package-enable-at-startup nil)
+
+(require 'org)
+(org-babel-load-file "~/.emacs.d/init.org")
+
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+;;; init.el ends here
